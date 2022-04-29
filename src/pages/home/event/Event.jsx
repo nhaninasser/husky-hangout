b8bc3ce2caa76e1555fbc8a572660c3b7e30a369
Sean <mckeags7@gmail.com>
2022-04-28 20:01:53 -0400

    Top bar test #1

---
+++ src/pages/home/event/Event.jsx
@@ -0,0 +1,18 @@
+import "./event.css";
+export default function Event() {
+    return (
+      <>
+        <Topbar />
+        <div className="event">
+           <div className="eventCover">
+                <img
+                  className="eventCoverImg"
+                  src=""
+                  alt=""
+                />
+            </div>
+        </div>
+        <Footer />
+      </>
+    );
+  }
