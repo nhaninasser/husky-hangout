3f74f40d4f3b036b398a6d0fe3264c3aed3c9ba2
tooqk4u <tooqk4u@protonmail.com>
2022-04-29 20:15:42 -0400

    singup form started

---
+++ client/src/components/Signup/index.js
@@ -0,0 +1,21 @@
+import React, { useState } from 'react';
+import { Form, Button, Alert } from 'react-bootstrap';
+import { useMutation } from '@apollo/react-hooks';
+import { ADD_USER } from '../utils/mutations';
+import Auth from '../utils/auth';
+
+const SignupForm = () => {
+  // set initial form state
+  const [userFormData, setUserFormData] = useState({ username: '', email: '', password: '' });
+  // set state for form validation
+  const [validated] = useState(false);
+  // set state for alert
+  const [showAlert, setShowAlert] = useState(false);
+  // sets up add user mutation hook to call the add user mutation
+  const [addUser, {error}] = useMutation(ADD_USER);
+
+  const handleInputChange = (event) => {
+    const { name, value } = event.target;
+    setUserFormData({ ...userFormData, [name]: value });
+  };
+}
