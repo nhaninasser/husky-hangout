daaa865fbb4fbfb10514b7a23062ed1a2335058a
Zachsteuer98 <Zach.Steuer98@gmail.com>
2022-05-03 22:51:40 -0400

    Created Header

--- client/src/App.js
+++ client/src/App.js
@@ -10,6 +10,7 @@ import { BrowserRouter, Routes, Route } from "react-router-dom";
 import FixedBottomNavigation from "./components/Footer";
 import EventsMobile from "./components/Events";
 import Login from "./components/Login";
+import AccountMenu from "./components/Header";
 // import Categories from '../src/components/Categories';
 import { StoreProvider } from "./utils/GlobalState";
 
@@ -29,6 +30,7 @@ function App() {
     <ApolloProvider client={client}>
       <StoreProvider>
         <BrowserRouter>
+          <AccountMenu></AccountMenu>
           <div className="body main-background-color">
             {/* /* <Categories } */}
             <Routes>
