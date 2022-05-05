b466279985c716dadd7716378f2f665562c3f143
James Barry <jbm03003@gmail.com>
2022-05-05 16:15:42 -0400

    updated auth in app.js

--- client/src/App.js
+++ client/src/App.js
@@ -6,6 +6,8 @@ import {
   ApolloProvider,
   createHttpLink,
 } from "@apollo/client";
+import { setContext } from '@apollo/client/link/context';
+
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import FixedBottomNavigation from "./components/Footer";
 import Login from "./components/Login";
@@ -18,8 +20,18 @@ const httpLink = createHttpLink({
   uri: "http://localhost:3001/graphql",
 });
 
+const authLink = setContext((_, { headers }) => {
+  const token = localStorage.getItem('id_token');
+  return {
+    headers: {
+      ...headers,
+      authorization: token ? `Bearer ${token}` : '',
+    },
+  };
+});
+
 const client = new ApolloClient({
-  link: httpLink,
+  link: authLink.concat(httpLink),
   cache: new InMemoryCache(),
 });
 
